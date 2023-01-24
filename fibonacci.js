5d2413ae8402fcbbbe2b30142ec32f80333297b9
Armando <armando.ortiz.jr.19@outlook.com>
2023-01-24 15:12:55 -0600

    Create recursiv e function version for solving fibonacci sequence

--- fibonacci.js
+++ fibonacci.js
@@ -14,4 +14,22 @@ function fibs(fibNum) {
   return currFibArr;
 }
 
-console.log(fibs(8));
+//Recursive version of fibonacci sequence
+function fibsRec(fibNum) {
+  if (fibNum === 2) {
+    return [0, 1];
+  }
+  if (fibNum === 1) {
+    return [0];
+  }
+  if (fibNum < 1) {
+    return null;
+  }
+
+  const previousFibArr = fibsRec(fibNum - 1);
+  const lastIndex = previousFibArr.length - 1;
+  const secondToLastIndex = previousFibArr.length - 2;
+  previousFibArr.push(previousFibArr[lastIndex] + previousFibArr[secondToLastIndex]);
+
+  return previousFibArr;
+}
